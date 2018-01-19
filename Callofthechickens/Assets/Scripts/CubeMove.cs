f630f7830e9ece0e10a527889fe553b0c687b85f
Krystle <krystle.hawkes@gmail.com>
2018-01-18 19:29:43 -0700

    created movement
    Camera Follow, CubeMove, Scripts
    Added Test Level Texture

---
+++ Callofthechickens/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+
+	public float transX;
+	public float transY;
+	public float transZ;
+
+	public float rotX;
+	public float rotY;
+	public float rotZ;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 transform.Translate(transX,transY,transZ);
+		 transform.Rotate(rotX,rotY,rotZ);
+	}
+}
